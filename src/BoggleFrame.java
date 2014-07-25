337fd46ff3287aa266c97271227770e9d0ac89e1
n00bed <anildgl@gmail.com>
2014-07-24 22:38:44 -0600

    Added more GUI stuff

--- src/BoggleFrame.java
+++ src/BoggleFrame.java
@@ -7,6 +7,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,19 +23,17 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 
-
-
 @SuppressWarnings("serial")
 public class BoggleFrame extends JFrame
 {
 	private JPanel startScreenPanel; 
 	private JPanel testPanel; 
-	
+
 	private int mySeconds;
 	private int myGameLength;
 	private JProgressBar myProgress;
 	private javax.swing.Timer myTimer;
-	
+
 	private JButton btnStartGame; 
 	private JTextArea txtrHighScore;
 	private JPanel gamePanel;
@@ -51,35 +50,34 @@ public class BoggleFrame extends JFrame
 	private JTextField textField;
 	private JProgressBar progressStatusBar;
 	private Timer timer; 
-	
-	
-	
- 
-	
+
+
+
 	public BoggleFrame() {
 		//Card Layout to hold both main screen and game screen and set the size of the screen  
 		getContentPane().setLayout(new CardLayout(0, 0));
-	    this.setSize(600, 500);
-		
-	    //Main screen that has start game button, high score and name of the game
+		this.setSize(600, 500);
+
+		//Main screen that has start game button, high score and name of the game
 		startScreenPanel = new JPanel();
 		getContentPane().add(startScreenPanel, "name_1406241583084739000");
 		startScreenPanel.setLayout(null);
-		
+
 		//Start button to start the game; 
 		btnStartGame = new JButton("START GAME");
 		btnStartGame.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				startScreenPanel.setVisible(false);
 				gamePanel.setVisible(true);
-				
+
 			}
 		});
 		btnStartGame.setFont(new Font("American Typewriter", Font.BOLD, 30));
 		btnStartGame.setBounds(52, 204, 246, 221);
 		startScreenPanel.add(btnStartGame);
-		
-		
+
+
 		//Text area that displays the games name "THE ULTIMATE BOGGLE"
 		JTextArea gameNameLabel = new JTextArea();
 		gameNameLabel.setEditable(false);
@@ -90,7 +88,7 @@ public class BoggleFrame extends JFrame
 		gameNameLabel.setText("THE ULTIMATE\n       BOGGLE");
 		gameNameLabel.setBounds(14, 27, 357, 155);
 		startScreenPanel.add(gameNameLabel);
-		
+
 		//Text area to display HighScore
 		txtrHighScore = new JTextArea();
 		txtrHighScore.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
@@ -100,82 +98,115 @@ public class BoggleFrame extends JFrame
 		txtrHighScore.setText("      HIGH SCORE");
 		txtrHighScore.setBounds(383, 27, 184, 392);
 		startScreenPanel.add(txtrHighScore);
-		
+
+		//Actual game screen 
 		gamePanel = new JPanel();
 		getContentPane().add(gamePanel, "name_1406249004660584000");
 		gamePanel.setLayout(new BorderLayout(0, 0));
-		
+
+		//Holds the panel for list entered by player and score while game is running 
 		wordListArea = new JPanel();
 		wordListArea.setPreferredSize(new Dimension(200, 350));
 		gamePanel.add(wordListArea, BorderLayout.WEST);
 		wordListArea.setLayout(null);
-		
+
+		//Label for holding score during gameplay 
 		lblScore = new JLabel("  SCORE:");
 		lblScore.setFont(new Font("American Typewriter", Font.BOLD, 16));
 		lblScore.setHorizontalAlignment(SwingConstants.LEFT);
 		lblScore.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
 		lblScore.setBounds(6, 30, 184, 50);
 		wordListArea.add(lblScore);
-		
+
+		//Actual opanel that holds the word list 
 		wordListPanel = new JPanel();
 		wordListPanel.setBorder(new TitledBorder(null, "WordList ", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		wordListPanel.setBounds(6, 80, 188, 337);
 		wordListArea.add(wordListPanel);
 		wordListPanel.setLayout(new GridLayout(1, 0, 0, 0));
-		
+
+		//Text area where entered words are saved 
 		wordListTextArea = new JTextArea();
 		wordListPanel.add(wordListTextArea);
-		
+
+		//provides Scrollbar to  the wordlist text area
 		scroll = new JScrollPane(wordListTextArea);
 		wordListPanel.add(scroll); 
-		
+
+		//Progress bar panel to hold  the status bar 
 		progressBar = new JPanel();
 		progressBar.setPreferredSize(new Dimension(10, 30));
 		gamePanel.add(progressBar, BorderLayout.NORTH);
 		
+		JLabel lblTimeLeft = new JLabel("TIME LEFT:");
+		progressBar.add(lblTimeLeft);
+
+		//progress bar to keep track of time 
 		progressStatusBar = new JProgressBar();
 		progressBar.add(progressStatusBar);
-		  progressStatusBar.setValue(100);
-	        ActionListener listener = new ActionListener() {
-	            int counter = 100;
-	            public void actionPerformed(ActionEvent ae) {
-	                counter--;
-	                progressStatusBar.setValue(counter);
-	                if (counter<1) {
-	                    JOptionPane.showMessageDialog(null, "GameOver");
-	                    timer.stop();
-	                } 
-	            }
-	        };
-	        timer = new Timer(1000, listener);
-	        timer.start();
-		
-		
+		progressStatusBar.setValue(100);
+		ActionListener listener = new ActionListener() {
+			int counter = 10;
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				counter--;
+				progressStatusBar.setValue(counter);
+				if (counter<1) {
+					JOptionPane.showMessageDialog(null, "GameOver");
+					timer.stop();
+				} 
+			}
+		};
+		timer = new Timer(1000, listener);
+		timer.start();
+
+
+		//Panel for actual game 
 		gameArea = new JPanel();
+		gameArea.setPreferredSize(new Dimension(200,200));
 		gamePanel.add(gameArea, BorderLayout.CENTER);
+		gameArea.setLayout(new GridLayout(4, 4, 5, 5));
+		gameArea.setBorder(BorderFactory.createEmptyBorder(30,30,30,30));
 		
+		
+	
+		JLabel[] squareButton = new JLabel[16];
+		
+		for(int i = 0; i < 16; i++)
+		{
+			//squareButton[i] = new JLabel("hello"); 
+			squareButton[i] = new JLabel(); 
+			squareButton[i].setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
+			gameArea.add(squareButton[i]);
+		}
+		
+
+		//Panel to hold Enter Word label, textfield and done button 
 		wordInputArea = new JPanel(); 
 		wordInputArea.setPreferredSize(new Dimension(10, 25));
 		gamePanel.add(wordInputArea, BorderLayout.SOUTH);
 		wordInputArea.setLayout(null);
-		
+
+		//Enterword label 
 		lblEnterWord = new JLabel("Enter Word:");
 		lblEnterWord.setHorizontalAlignment(SwingConstants.TRAILING);
 		lblEnterWord.setFont(new Font("American Typewriter", Font.BOLD, 16));
 		lblEnterWord.setBounds(6, 0, 140, 20);
 		wordInputArea.add(lblEnterWord);
-		
+
+		//Textfield for players to type word
 		textField = new JTextField();
 		textField.setBounds(169, 0, 300, 20);
 		wordInputArea.add(textField);
 		textField.setColumns(10);
-		
+
+		//Done button to end the game
 		JButton btnDone = new JButton("Done");
 		btnDone.setBounds(477, 0, 117, 20);
 		wordInputArea.add(btnDone);
-		
-		
-		
-		
+
+
+
+
 	}
 }
